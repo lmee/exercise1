9fd23d01749eecd1d552ac11a3916f9eaa98508f
lmee <lmee_mail@163.com>
2016-04-01 22:57:52 +0800

    add some codes

--- BasicModule.py
+++ BasicModule.py
@@ -1,3 +1,15 @@
 # /usr/bin/python3
 
 print("This is a script file,it's will upload to github, so cool!")
+todos = 'get,gloves,get mask,give cat vitamins,call ambulance'
+print(todos.split(','))
+
+str1='Jieer'
+str2='is good!'
+strtem= str1+" "+str2+"........."
+
+print(strtem.count('is'))
+print(strtem.startswith("Jieer1"))
+print(strtem.endswith("..."))
+print(strtem.strip('.'))
+print(strtem[1:5])
