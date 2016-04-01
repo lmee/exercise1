88a05aee02d89270b82e02e23105bac6b9c74d0e
lmee <lmee_mail@163.com>
2016-04-01 23:19:00 +0800

    commit some codes

--- BasicModule.py
+++ BasicModule.py
@@ -2,9 +2,11 @@
 
 print("This is a script file,it's will upload to github, so cool!")
 todos = 'get,gloves,get mask,give cat vitamins,call ambulance'
-print(todos.split(','))
+todos_list=todos.split(',')
+print(todos_list)
+print(','.join(todos_list))
 
-str1='Jieer'
+str1='jieer'
 str2='is good!'
 strtem= str1+" "+str2+"........."
 
@@ -12,4 +14,25 @@ print(strtem.count('is'))
 print(strtem.startswith("Jieer1"))
 print(strtem.endswith("..."))
 print(strtem.strip('.'))
-print(strtem[1:5])
+print(strtem[1:-5])
+
+print(strtem.capitalize())
+
+print(strtem.title())
+
+print(strtem.upper())
+
+print(strtem.swapcase())
+print(strtem.center(5))
+
+print(strtem.replace(' ',','))
+
+print(len("Jieer" * 3))
+
+print(str(98.5))
+
+print('''jieer is good boy !!''')
+
+print(int("34"))
+
+print(float('1.0e4'))
